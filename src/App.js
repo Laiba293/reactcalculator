1bcc823ea54738bb54557b6c600c27e742fff351
laiba <laiba.khalid1114@gmail.com>
2023-02-27 15:42:11 +0500

    initial

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
+import React from "react";
+import  {add , sub , div , mult } from "./Calc";
+function App(){
+    return(
+        <>
+           <ul>
+                <li> {add(40,4)} </li>
+                <li> {sub(30,3)}</li>
+                <li>  {div(10,4)}</li>
+                <li> {mult(3,5)}</li>
+           </ul>
+        </>
+    );
+} export default App;
